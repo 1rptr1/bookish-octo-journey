d653407c01b1e730e14a87625eec276b627055fc
1rptr1 <sauravbhr712@gmail.com>
2026-01-18 19:09:57 +0530

    removed gpt4all

--- llm/providers/client-factory.js
+++ llm/providers/client-factory.js
@@ -1,6 +1,6 @@
 import { OllamaClient } from "./ollama.js";
 import { LMStudioClient } from "./lmstudio.js";
-import { GPT4AllClient } from "./gpt4all.js";
+//import { GPT4AllClient } from "./gpt4all.js";
 
 export function createLLMClient(provider, model) {
     switch (provider) {
@@ -10,8 +10,8 @@ export function createLLMClient(provider, model) {
         case "lmstudio":
             return new LMStudioClient(model);
 
-        case "gpt4all":
-            return new GPT4AllClient(model);
+        // case "gpt4all":
+        //     return new GPT4AllClient(model);
 
         default:
             throw new Error(`Unknown provider: ${provider}`);
